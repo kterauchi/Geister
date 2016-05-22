c8cd05627bb19db01bc56bff5c7f6e0a2cc94de8
Keiichiro <terauchi@tri-arrows.jp>
2016-05-22 16:10:04 +0900

    initial commit

---
+++ Geister/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Geister
+//
+//  Created by Keiichiro on 2016/05/22.
+//  Copyright © 2016年 Keiichiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
